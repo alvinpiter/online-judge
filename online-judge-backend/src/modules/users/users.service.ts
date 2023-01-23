5457f8d3ff3a01fa4060f2f4a8ecefb7e8dcdfc1
Alvin Piter <alpiter10@gmail.com>
2023-01-23 11:51:21 +0800

    Implement minimal user registration mechanism

--- online-judge-backend/src/modules/users/users.service.ts
+++ online-judge-backend/src/modules/users/users.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './user.entity';
+import { UserRegistrationDto } from './data-transfer-objects/user-registration.dto';
+import { User, UserRole } from './user.entity';
 
 @Injectable()
 export class UsersService {
@@ -11,5 +12,14 @@ export class UsersService {
 
   async findByUsername(username: string): Promise<User | undefined> {
     return this.usersRepository.findOneBy({ username });
+  }
+
+  async registerUser(userRegistrationDto: UserRegistrationDto): Promise<User> {
+    const user = new User();
+    user.username = userRegistrationDto.username;
+    user.hashedPassword = userRegistrationDto.password;
+    user.role = UserRole.USER;
+
+    return this.usersRepository.save(user);
   }
 }
