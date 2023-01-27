83ea151b05a63e365ebd3a7a673bf937858198b5
Alvin Piter <alpiter10@gmail.com>
2023-01-27 16:30:55 +0800

    Remove unnecessary code

--- online-judge-backend/src/modules/health-check/health-check.controller.ts
+++ online-judge-backend/src/modules/health-check/health-check.controller.ts
@@ -1,18 +1,9 @@
-import { InjectRedis } from '@liaoliaots/nestjs-redis';
 import { Controller, Get } from '@nestjs/common';
-import { Redis } from 'ioredis';
 
 @Controller('api')
 export class HealthCheckController {
-  constructor(@InjectRedis() private readonly redisClient: Redis) {}
-
   @Get('health-check')
   healthCheck() {
     return { health: 'OK' };
-  }
-
-  @Get('/redis/ping')
-  async redisPing() {
-    return this.redisClient.ping();
   }
 }
