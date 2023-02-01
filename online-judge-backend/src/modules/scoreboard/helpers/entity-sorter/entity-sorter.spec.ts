d62e031d024e44be10c53028c52281771850055a
Alvin Piter <alpiter10@gmail.com>
2023-02-01 16:20:40 +0800

    Implement EntitySorterService.getPaginatedSortedEntities

--- online-judge-backend/src/modules/scoreboard/helpers/entity-sorter/entity-sorter.spec.ts
+++ online-judge-backend/src/modules/scoreboard/helpers/entity-sorter/entity-sorter.spec.ts
@@ -1,5 +1,11 @@
+import { pick } from 'lodash';
 import { createUser } from 'src/lib/tests/createUser';
+import {
+  SortedSetOrder,
+  SortedSetPaginatedQueryBuilder,
+} from 'src/modules/cache/sorted-set/sorted-set-paginated-query-builder';
 import { SortedSetService } from 'src/modules/cache/sorted-set/sorted-set.service';
+import { OffsetPaginationService } from 'src/modules/pagination/offset-pagination.service';
 import { User } from 'src/modules/users/user.entity';
 import { EntitySorterService } from './entity-sorter';
 import { UserScoringSchema } from './test-mocks/interfaces';
@@ -7,17 +13,96 @@ import { MockedEntitiyIdentifierMapper } from './test-mocks/mocked-entity-identi
 import { MockedEntityScoreCalculator } from './test-mocks/mocked-entity-score-calculator';
 
 describe(EntitySorterService.name, () => {
+  const order = SortedSetOrder.SCORE_DESC;
   const sortedSetService = SortedSetService.prototype;
   const entityIdentifierMapper = new MockedEntitiyIdentifierMapper();
   const entityScoreCalculator = new MockedEntityScoreCalculator();
+  const offsetPaginationService = OffsetPaginationService.prototype;
   let service: EntitySorterService<User, UserScoringSchema>;
 
   beforeEach(() => {
     service = new EntitySorterService<User, UserScoringSchema>(
+      order,
       sortedSetService,
       entityIdentifierMapper,
       entityScoreCalculator,
+      offsetPaginationService,
     );
+  });
+
+  describe('getPaginatedSortedEntities', () => {
+    const user1 = {
+      entity: createUser(1),
+      member: 'user:1',
+      score: 1,
+      numericScore: 1,
+      schematicScore: { numberOfFriends: 1, numberOfEnemies: 1 },
+      rank: 1,
+    };
+
+    const user2 = {
+      entity: createUser(2),
+      member: 'user:2',
+      score: 2,
+      numericScore: 2,
+      schematicScore: { numberOfFriends: 2, numberOfEnemies: 2 },
+      rank: 2,
+    };
+
+    const fieldsFromPaginationService = ['member', 'score', 'rank'];
+    const fieldsInFinalResult = [
+      'entity',
+      'numericScore',
+      'schematicScore',
+      'rank',
+    ];
+
+    const offsetLimit = { offset: 1, limit: 2 };
+    const paginationMeta = { ...offsetLimit, total: 10 };
+
+    it('returns the correct result', async () => {
+      jest.spyOn(offsetPaginationService, 'paginate').mockResolvedValue({
+        data: [
+          pick(user1, ...fieldsFromPaginationService),
+          pick(user2, ...fieldsFromPaginationService),
+        ],
+        meta: paginationMeta,
+      });
+
+      jest
+        .spyOn(entityIdentifierMapper, 'fromIdentifiers')
+        .mockResolvedValue([user1.entity, user2.entity]);
+
+      jest
+        .spyOn(entityScoreCalculator, 'getSchematicScore')
+        .mockImplementation(async (numericScore) => {
+          if (numericScore === 1) {
+            return user1.schematicScore;
+          } else {
+            return user2.schematicScore;
+          }
+        });
+
+      const result = await service.getPaginatedSortedEntites(offsetLimit);
+
+      expect(offsetPaginationService.paginate).toHaveBeenCalledWith(
+        expect.any(SortedSetPaginatedQueryBuilder),
+        offsetLimit,
+      );
+      expect(entityIdentifierMapper.fromIdentifiers).toHaveBeenCalledWith([
+        'user:1',
+        'user:2',
+      ]);
+      expect(entityScoreCalculator.getSchematicScore).toHaveBeenCalledWith(1);
+      expect(entityScoreCalculator.getSchematicScore).toHaveBeenCalledWith(2);
+      expect(result).toEqual({
+        data: [
+          pick(user1, ...fieldsInFinalResult),
+          pick(user2, ...fieldsInFinalResult),
+        ],
+        meta: paginationMeta,
+      });
+    });
   });
 
   describe('updateEntityScore', () => {
--- online-judge-backend/src/modules/scoreboard/helpers/entity-sorter/entity-sorter.ts
+++ online-judge-backend/src/modules/scoreboard/helpers/entity-sorter/entity-sorter.ts
@@ -1,15 +1,71 @@
+import { map, zip } from 'lodash';
+import {
+  SortedSetOrder,
+  SortedSetPaginatedQueryBuilder,
+} from 'src/modules/cache/sorted-set/sorted-set-paginated-query-builder';
 import { SortedSetService } from 'src/modules/cache/sorted-set/sorted-set.service';
-import { EntityIdentifierMapper, EntityScoreCalculator } from './interfaces';
+import { OffsetPaginationResult } from 'src/modules/pagination/offset-pagination.interface';
+import { OffsetPaginationService } from 'src/modules/pagination/offset-pagination.service';
+import {
+  EntityIdentifierMapper,
+  EntityScoreCalculator,
+  SortedEntitiesPaginationParameter,
+  SortedEntity,
+} from './interfaces';
 
 export class EntitySorterService<Entity, ScoringSchema> {
   constructor(
+    private readonly order: SortedSetOrder,
     private readonly sortedSetService: SortedSetService,
     private readonly entityIdentifierMapper: EntityIdentifierMapper<Entity>,
     private readonly entityScoreCalculator: EntityScoreCalculator<
       Entity,
       ScoringSchema
     >,
+    private readonly offsetPaginationService: OffsetPaginationService,
   ) {}
+
+  async getPaginatedSortedEntites(
+    parameters: SortedEntitiesPaginationParameter<Entity>,
+  ): Promise<OffsetPaginationResult<SortedEntity<Entity, ScoringSchema>>> {
+    const qb = new SortedSetPaginatedQueryBuilder(
+      this.sortedSetService,
+      this.order,
+    );
+
+    if (parameters.entity) {
+      const memberIdentifer = await this.entityIdentifierMapper.toIdentifier(
+        parameters.entity,
+      );
+      qb.addMemberFilter(memberIdentifer);
+    }
+
+    const { data: rawResults, meta } =
+      await this.offsetPaginationService.paginate(qb, {
+        offset: parameters.offset,
+        limit: parameters.limit,
+      });
+
+    const entities = await this.entityIdentifierMapper.fromIdentifiers(
+      map(rawResults, 'member'),
+    );
+
+    const results: SortedEntity<Entity, ScoringSchema>[] = await Promise.all(
+      zip(rawResults, entities).map(async ([rawResult, entity]) => ({
+        entity,
+        rank: rawResult.rank,
+        numericScore: rawResult.score,
+        schematicScore: await this.entityScoreCalculator.getSchematicScore(
+          rawResult.score,
+        ),
+      })),
+    );
+
+    return {
+      data: results,
+      meta,
+    };
+  }
 
   async updateEntityScore(entity: Entity) {
     const identifier = await this.entityIdentifierMapper.toIdentifier(entity);
