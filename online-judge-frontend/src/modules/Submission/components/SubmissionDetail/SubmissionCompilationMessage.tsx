19759a86d7561e8064384035c2a3e7a8fda68c9b
Alvin Piter <alpiter10@gmail.com>
2023-02-09 16:29:27 +0800

    Fix compilation message component

--- online-judge-frontend/src/modules/Submission/components/SubmissionDetail/SubmissionCompilationMessage.tsx
+++ online-judge-frontend/src/modules/Submission/components/SubmissionDetail/SubmissionCompilationMessage.tsx
@@ -9,7 +9,7 @@ export const SubmissionCompilationMessage: FC<
   SubmissionCompilationMessageProps
 > = ({ compilationDetail }) => {
   return (
-    <pre style={{ backgroundColor: "#f1f1f1" }}>
+    <pre style={{ backgroundColor: "#f1f1f1", padding: 8 }}>
       {compilationDetail.message}
     </pre>
   );
