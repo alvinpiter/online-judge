b3e911aeea4e70ef9d5518ba3aa5f6e051c23110
Alvin Piter <alpiter10@gmail.com>
2023-02-10 09:13:45 +0800

    Add welcome message

--- online-judge-frontend/src/pages/HomePage.tsx
+++ online-judge-frontend/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
-import { Paper, Stack, Typography } from "@mui/material";
+import { Link, Paper, Stack, Typography } from "@mui/material";
 import { FC } from "react";
+import { ROUTES } from "../constants/Routes";
 import { useGetGlobalStatisticsRequest } from "../modules/Statistics/hooks/useGetGlobalStatisticsRequest";
 
 export const HomePage: FC = () => {
@@ -7,8 +8,33 @@ export const HomePage: FC = () => {
 
   return (
     <>
+      <Paper elevation={2} sx={{ flex: 1, padding: 2 }}>
+        <Typography variant="h3">Welcome to Online Judge!</Typography>
+
+        <Typography variant="body1" sx={{ mt: 2 }}>
+          {" "}
+          Features:{" "}
+        </Typography>
+
+        <Link href={ROUTES.USER_PROBLEMS_ROUTE.generatePath()}>
+          <Typography variant="body1">
+            Practice solving coding problems
+          </Typography>
+        </Link>
+
+        <Link href={ROUTES.SUBMISSIONS_ROUTE.generatePath()}>
+          <Typography variant="body1"> See other users' solutions </Typography>
+        </Link>
+
+        <Link href={ROUTES.SCOREBOARD_ROUTE.generatePath()}>
+          <Typography variant="body1">
+            Compare your performance against other users
+          </Typography>
+        </Link>
+      </Paper>
+
       {globalStatistics && (
-        <Stack direction="row" spacing={2}>
+        <Stack direction="row" spacing={2} sx={{ mt: 2 }}>
           <Paper elevation={2} sx={{ padding: 2, flex: 1 }}>
             <Typography variant="h5">Number of users</Typography>
             <Typography variant="h4" sx={{ mt: 2 }}>
