50d8e41887c9a91b346469cfa6e9a3b39616e878
Alvin Piter <alpiter10@gmail.com>
2023-02-13 15:04:44 +0800

    Remove unnecessary code

--- online-judge-backend/src/modules/search/controllers/user-search.controller.ts
+++ online-judge-backend/src/modules/search/controllers/user-search.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { UserFormatter } from 'src/modules/users/formatters/user.formatter';
-import { UsersService } from 'src/modules/users/users.service';
 import { UserSearchService } from '../services/user-search-service/user-search.service';
 
 @Controller('api')
 export class UserSearchController {
   constructor(
     private readonly userSearchService: UserSearchService,
-    private readonly usersService: UsersService,
     private readonly userFormatter: UserFormatter,
   ) {}
 
@@ -19,15 +17,5 @@ export class UserSearchController {
       userSearchQueryParameters.query,
     );
     return users.map((user) => this.userFormatter.format(user));
-  }
-
-  @Get('upsert-search-suggestions')
-  async upsertSearchSuggestions() {
-    const users = await this.usersService.getAllUsers();
-    await Promise.all(
-      users.map((user) => this.userSearchService.upsertSuggestion(user.id)),
-    );
-
-    return 'ok';
   }
 }
