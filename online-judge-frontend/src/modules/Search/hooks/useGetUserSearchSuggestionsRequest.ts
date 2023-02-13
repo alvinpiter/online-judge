2369679f034c0dfae85f2d063ea7ae25506de417
Alvin Piter <alpiter10@gmail.com>
2023-02-13 14:05:03 +0800

    Remove unnecessary codes

--- online-judge-frontend/src/modules/Search/hooks/useGetUserSearchSuggestionsRequest.ts
+++
@@ -1,8 +0,0 @@
-import { config } from "../../../config";
-import { useHTTPGetRequest } from "../../../lib/http/useHTTPGetRequest";
-import { User } from "../../User/interface";
-
-export function useGetUserSearchSuggestionsRequest(usernamePrefix: string) {
-  const apiUrl = `${config.backendAPIURL}/user-search-suggestions?query=${usernamePrefix}`;
-  return useHTTPGetRequest<User[]>(apiUrl);
-}
