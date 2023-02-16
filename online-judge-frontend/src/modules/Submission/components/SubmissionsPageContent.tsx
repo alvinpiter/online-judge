63eb3ff5376d1680dd47b45fe6139db6b53b6477
Alvin Piter <alpiter10@gmail.com>
2023-02-16 13:55:55 +0800

    Fix Submissions page

--- online-judge-frontend/src/modules/Submission/components/SubmissionsPageContent.tsx
+++ online-judge-frontend/src/modules/Submission/components/SubmissionsPageContent.tsx
@@ -1,11 +1,13 @@
 import { Box, Pagination, Paper } from "@mui/material";
 import { ChangeEvent, FC } from "react";
+import { LoadingState } from "../../../lib/components/LoadingState";
 import { useSubmissionsContext } from "../contexts/SubmissionsContext/context";
 import { SubmissionsFilterForm } from "./SubmissionFilterForm/SubmissionsFilterForm";
 import { SubmissionsTable } from "./SubmissionsTable/SubmissionsTable";
 
 export const SubmissionsPageContent: FC = () => {
   const {
+    isLoadingEntities: isLoadingSubmissions,
     entities: submissions,
     currentPage,
     filter,
@@ -23,27 +25,33 @@ export const SubmissionsPageContent: FC = () => {
 
   return (
     <Box sx={{ display: "flex", mt: 2, mb: 4 }}>
-      <Box sx={{ flexGrow: 2 }}>
-        <Paper elevation={2}>
-          <SubmissionsTable submissions={submissions} />
-        </Paper>
+      <Box sx={{ flex: 9 }}>
+        {isLoadingSubmissions ? (
+          <LoadingState />
+        ) : (
+          <>
+            <Paper elevation={2}>
+              <SubmissionsTable submissions={submissions} />
+            </Paper>
 
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            mt: 1,
-          }}
-        >
-          <Pagination
-            page={currentPage}
-            count={numberOfPages}
-            onChange={handlePaginationChange}
-          />
-        </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                mt: 1,
+              }}
+            >
+              <Pagination
+                page={currentPage}
+                count={numberOfPages}
+                onChange={handlePaginationChange}
+              />
+            </Box>
+          </>
+        )}
       </Box>
 
-      <Box sx={{ flexGrow: 1, ml: 2 }}>
+      <Box sx={{ flex: 3, ml: 2 }}>
         <Paper elevation={2} sx={{ padding: 2 }}>
           <SubmissionsFilterForm
             initialFilter={filter}
