897e3ddb638b9a0c086765fa79d5f34f1bec20c5
Alvin Piter <alpiter10@gmail.com>
2023-02-19 10:46:15 +0800

    Script for deploying database-migrator

---
+++ deployment/scripts/deploy-database-migrator.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../../online-judge-backend
+
+image_tag=alvinpiter/online-judge-database-migrator
+
+docker build -f database-migrator.Dockerfile -t $image_tag .
+docker push $image_tag
+
+cd ../deployment/manifests/database-migrator
+set -a && source .env && envsubst < manifest.yml | kubectl apply -f -
