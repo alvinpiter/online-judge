55df7afa76a28b8d04dc61054b223b0f6151ca51
Alvin Piter <alpiter10@gmail.com>
2023-02-21 12:54:16 +0800

    Fix `submission_run_details` table (#91)
    * Add outputFileKey to submission_run_details table
    
    * Save outputFileKey to submission_run_details table
    
    * Display submission's output file in FE
    
    * Set MySQL sts storage to 5Gi
    
    * Fix submission's output file key

--- online-judge-backend/src/modules/submissions/formatters/submission.formatter.ts
+++ online-judge-backend/src/modules/submissions/formatters/submission.formatter.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { ObjectStorageService } from 'src/modules/object-storage/object-storage.service';
 import { ProblemTestCasesFormatter } from 'src/modules/problems/formatters/problem-test-cases.formatter';
 import { UserFormatter } from 'src/modules/users/formatters/user.formatter';
+import { SubmissionRunDetail } from '../entities/submission-run-detail.entity';
 import { SubmissionWithResolvedProperty } from '../interfaces/submission-with-resolved-property';
 import { SubmissionWithDetails } from '../interfaces/submition-with-details';
 
@@ -9,6 +11,7 @@ export class SubmissionFormatter {
   constructor(
     private readonly userFormatter: UserFormatter,
     private readonly problemTestCaseFormatter: ProblemTestCasesFormatter,
+    private readonly objectStorageService: ObjectStorageService,
   ) {}
 
   format(submission: SubmissionWithResolvedProperty) {
@@ -32,6 +35,7 @@ export class SubmissionFormatter {
         testCase: await this.problemTestCaseFormatter.formatProblemTestCase(
           detail.testCase,
         ),
+        outputFile: await this.formatOutputFile(detail),
       })),
     );
 
@@ -40,5 +44,19 @@ export class SubmissionFormatter {
       compilationDetail: submissionWithDetails.compilationDetail,
       runDetails,
     };
+  }
+
+  private async formatOutputFile(detail: SubmissionRunDetail) {
+    if (detail.output.length > 0) {
+      return null;
+    } else {
+      return {
+        name: `${detail.testCase.inputFileName.replace(
+          '.txt',
+          '',
+        )}_actual_output.txt`,
+        url: await this.objectStorageService.getSignedUrl(detail.outputFileKey),
+      };
+    }
   }
 }
